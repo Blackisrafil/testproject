21faa3e0983cfdb2d6dc7a731a905c5c200f26ee
Adnan Mirza <adnan.mirza@mnetics.co.uk>
2021-09-03 14:52:25 +0100

    Use player class for both Imp and Adventurer

--- practice projects/Exercise 1.py
+++ practice projects/Exercise 1.py
@@ -2,49 +2,61 @@ from random import randrange
 
 
 class Player:
-    def __init__(self, level, current_xp, hp, strength):
-        self.level = level
-        self.current_xp = current_xp
-        self.hp = hp
-        self.strength = strength
+    # Set defaults for new player classes
+    def __init__(self):
+        self.level = 1
+        self.current_xp = 0
+        self.hp = 20
+        self.strength = randrange(3, 5)
 
-    def roll_stats(self):
-        self.strength = randrange(3, 9), (4, 10), (5, 11)
+    # Function to calculate attack based on level
+    def roll_attack(self):
+        self.strength = randrange(2 + self.level, 4 + self.level)
+        return self.strength
 
     def get_stats(self):
         return {"level": self.level, "hp": self.hp, "strength": self.strength}
 
+    def add_exp(self, exp):
+        self.current_xp += exp
+        # Check if level up is needed
+        leveled_up = False
+        if self.current_xp >= 10 & self.current_xp < 20:
+            self.level = 2
+            leveled_up = True
+        elif self.current_xp >= 20 & self.current_xp < 30:
+            self.level = 3
+            leveled_up = True
 
-current_xp = 0
+        if leveled_up:
+            print(f"\nYou have leveled up to {self.level}!")
 
-imp_hp = 20
+
+# New player
+Adventurer = Player()
+# New enemy
+Imp = Player()
+
 
 print("You enter a cave to eliminate the demons. You have been ordered by a nearby towns Mayor to complete this task.")
 print("As you enter a cave, the first room contains a small, flying creature. You ready your iron blade.")
 input("\nEnter any key to commence battle: ")
 print("\nThe small creature see you and rushes to attack!")
 
-while imp_hp >= 1 or Adventurer.hp >= 1:
-    imp_strength = randrange(3, 5)
-    if current_xp == 0:
-        Adventurer = Player(1, current_xp, 20, randrange(3, 9))
-    if current_xp >= 10:
-        Adventurer = Player(2, current_xp, 31, randrange(4, 10))
-    if current_xp >= 20:
-        Adventurer = Player(3, current_xp, 39, randrange(5, 11))
-    # Not sure if this is the best way to execute the code as the player levels up. Did it like this so the random
-    # value can be recognized by the While loop.
-    input("Enter a key to roll an attack: ")
-    imp_hp -= Adventurer.strength
-    print("You inflict:", Adventurer.strength, "damage. The Imp has", imp_hp, "health remaining.")
-    Adventurer.hp -= imp_strength
-    print("You take:", imp_strength, "damage. You have", Adventurer.hp, "health remaining.")
-    if imp_hp <= 1:
+while Imp.hp >= 1 or Adventurer.hp >= 1:
+    # Execute function in player to get random value
+    input("\nEnter a key to roll an attack: ")
+    Imp.hp -= Adventurer.roll_attack()
+    print("You inflict:", Adventurer.strength, "damage. The Imp has", Imp.hp, "health remaining.")
+    # Check if Imp is still alive
+    if Imp.hp <= 1:
         break
+    Adventurer.hp -= Imp.roll_attack()
+    print("You take:", Imp.strength, "damage. You have", Adventurer.hp, "health remaining.")
     if Adventurer.hp <= 1:
-        print("You have fallen...")
+        print("\nYou have fallen...")
         exit()
 
-current_xp += 5
-print("You have won the battle and gained 5 XP!")
-print("Current XP:", + current_xp)
+Adventurer.add_exp(25)
+print("\nYou have won the battle and gained 5 XP!")
+print(f"Current XP: {Adventurer.current_xp}")
